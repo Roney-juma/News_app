b4be53adaeec82b61b79d329d4f870a630c9e783
Roney-juma <ochirone10@gmail.com>
2021-06-07 13:03:19 +0300

    created variable news_source and called get_sources function

--- app/views.py
+++ app/views.py
@@ -1,11 +1,19 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_sources,get_top_headlines,get_source_news,get_category_news,search_news
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    news_sources = get_sources()
+    top_headlines=get_top_headlines()[0:15]
+
+    search_article = request.args.get('news_query')
+    if search_article:
+        return redirect(url_for('.search',news_article=search_article))
+    else:    
+        return render_template('index.html',sources = news_sources,headlines = top_headlines)
