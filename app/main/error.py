d1667dd2c25a93df4f10b9c9e34f13efb6e147a2
Roney-juma <ochirone10@gmail.com>
2021-06-08 20:47:37 +0300

    made correction on error.py

--- app/main/error.py
+++ app/main/error.py
@@ -1,25 +1,26 @@
 from flask import render_template
-from app import main
+from . import main
 
-@main.errorhandler(403)
+@main.app_errorhandler(403)
 def forbidden_access(error):
     '''
     Function to handles 403 error ie forbidden access
     '''
-    return render_template('errors.html',error='page')
+    
+    return render_template('error.html',error ='page')
 
 
-@main.errorhandler(404)
+@main.app_errorhandler(404)
 def four_Ow_four(error):
     '''
     Function to handles 404 error ie page not found
     '''
-    return render_template('errors.html',error='page')
+    return render_template('error.html',error='page')
 
 
-@main.errorhandler(500)
+@main.app_errorhandler(500)
 def server_error(error):
     '''
     Function to handles 500 error ie server error
     '''
-    return render_template('errors.html',error='page')
+    return render_template('error.html',error='page')
